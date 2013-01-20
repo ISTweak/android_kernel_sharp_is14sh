5650b0ae5b760eb3ac81cb1bffcbde8ab89afc54
ISTweak <istweak@d-o-b.org>
2013-01-20 14:56:11 +0900

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 9 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 PREEMPT Wed Jan 2 23:38:19 JST 2013"
+#define LINUX_COMPILE_TIME "23:38:19"
+#define LINUX_COMPILE_BY "love"
+#define LINUX_COMPILE_HOST "marijuana"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.6.3 (Sourcery CodeBench Lite 2012.03-56) "
---
+++ include/sharp/sh_android_usb_desc_str.h
@@ -0,0 +1,21 @@
+/* kernel/include/sharp/sh_android_usb_desc_str.h
+ *
+ * Copyright (C) 2011 SHARP CORPORATION
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __SH_ANDROID_USB_DESC_STR_H
+#define __SH_ANDROID_USB_DESC_STR_H
+
+#include "sh_android_usb_desc_str_deckard_af21.h"
+
+#endif /* __SH_ANDROID_USB_DESC_STR_H */
